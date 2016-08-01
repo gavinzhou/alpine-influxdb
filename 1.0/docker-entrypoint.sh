bf4d4df35e600cf8a4f9d909fed68f3ca0eef575
gavinzhou <gavin.zhou@gmail.com>
2016-08-01 05:47:39 +0000

    init database telegraf with start docker

--- 1.0/docker-entrypoint.sh
+++ 1.0/docker-entrypoint.sh
@@ -1,8 +1,25 @@
-#!/bin/sh
-set -e
+#!/bin/bash
 
-if [ "${1:0:1}" = '-' ]; then
-    set -- influxd "$@"
+if [ -n "${PRE_CREATE_DB}" ]; then
+  exec influxd -config=/influxdb/etc/influxdb.conf &
+  _PID=$!
+  arr=$(echo ${PRE_CREATE_DB} | tr ";" "\n")
+
+  RET=1
+  while [[ RET -ne 0 ]]; do
+    echo "=> Waiting for confirmation of InfluxDB service startup ..."
+    sleep 3
+    curl -k http://127.0.0.1:8086/ping 2> /dev/null
+    RET=$?
+  done
+  echo ""
+
+  for x in $arr
+  do
+    echo "=> Creating database: ${x}"
+    influx -execute="create database \"${x}\""
+  done
+  kill -9 $_PID
 fi
 
 exec "/sbin/dumb-init" "$@"
